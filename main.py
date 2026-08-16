26a1b938c881fc0654037ac9b9f250a8e8a1821b
mava_1023 <valruizgiraldo@gmail.com>
2026-08-15 23:06:47 -0500

    More bugs

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ palabras_letras = [8,8,7,6,6,5,4,5,5,5,8,7,8,5,9,5,5]
 
 def pantalla_frente(jueguito):
     
-    abelardo = crear_imagen_con_tamanio(0,0,CANVAS_WIDTH, CANVAS_HEIGHT, "menu.png")
+    abelardo = canvas.crear_imagen_con_tamanio(0,0,CANVAS_WIDTH, CANVAS_HEIGHT, "menu.png")
     rect_texto = canvas.crear_rectangulo(140, 300, 265,340 , "#5F0826")
     rect_texto2 = canvas.crear_rectangulo(145, 305, 260, 335, "#FE7F2D")
     start= canvas.crear_texto(170,310, text = 'START',font = 'Century', font_size = 20, color ='#5F0826')
     while jueguito == 0:
         canvas.esperar_por_clic()
-        click.p = canvas.obtener_nuevos_clics_del_mouse()
+        click= canvas.obtener_nuevos_clics_del_mouse()
         x = click[0]
         y = click[1]
         objs = canvas.encontrar_superposicion(x,y,x+1, y+1)
