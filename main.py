d4f4d70bd7eb839fd434b8b88063e1e1b82eb7f8
gnoceram <gabriellafeliz11@gmail.com>
2026-08-15 22:35:07 -0500

    idk 2

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def frentota (palabras_abelardo, palabras_letras):
             for i in range (0,letras-1):
                 lista_word.append(palabra[i])
             
-            if new_l
+            
             
         
 
